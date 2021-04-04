c2fea9ca5e8201292546bf64f4546ef26d788d5d
kureharyosuke <ohhayo.com@gmail.com>
2021-04-05 00:29:18 +0900

    toBeNull, toBeUndefined, toBeDefined, toBeTruthy, toBeFalsy

---
+++ etc.js
@@ -0,0 +1,6 @@
+const etc = {
+  add: (num1, num2) => num1 + num2, // * 해서 에러남....
+  makeUser: (name, age) => ({ name, age, gender: undefined }),
+};
+
+module.exports = etc;
---
+++ etc.test.js
@@ -0,0 +1,44 @@
+const etc = require("./etc");
+
+// toBeNull
+// toBeUndefined
+// toBeDefined
+
+test("null is null", () => {
+  expect(null).toBeNull();
+});
+
+// toBeTruthy
+// toBeFalsy
+
+test("0 is false", () => {
+  expect(etc.add(1, -1)).toBeFalsy();
+});
+
+/**
+ *   ● 0 is false
+
+    expect(received).toBeFalsy()
+
+    Received: -1 
+ */
+
+// ?: 1 + -1 = 0?
+
+// test("0 is false", () => {
+//   expect(etc.add(0, 1)).toBeFalsy();
+// });
+
+// test("hello jest", () => {
+//   expect(etc.add("hello", "jest")).toBeFalsy();
+// });
+
+/**
+ *   expect(received).toBeFalsy()
+
+    Received: "hellojest"
+ */
+
+test("hello jest", () => {
+  expect(etc.add("hello", "jest")).toBeTruthy();
+});
